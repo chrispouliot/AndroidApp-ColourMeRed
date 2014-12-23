785aeea6d0f114aa062b2531b1bf9fabbea33781
moxuz <cristophepoug@gmail.com>
2014-12-23 15:21:12 -0800

    Colour Me Red 1.0

--- app/src/main/java/com/example/user/whatcolourisittoday/MainActivity.java
+++ app/src/main/java/com/example/user/whatcolourisittoday/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.user.whatcolourisittoday;
 
+import android.app.ActionBar;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +22,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Thread t = new Thread() {
         @Override
